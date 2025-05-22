3643c88c4e6b83eaa65c4bed5adbd5199ece61e9
rishugoyal805 <rishugoyal16800@gmail.com>
2025-05-23 00:48:11 +0530

    formatter file

---
+++ agents/formatter.py
@@ -0,0 +1,6 @@
+def format_output(theme, short_script, long_script):
+    return f"""
+=== Theme: {theme} ===\n
+--- YouTube Shorts Script ---\n{short_script}\n
+--- Long Video Script ---\n{long_script}\n
+==============================\n"""
